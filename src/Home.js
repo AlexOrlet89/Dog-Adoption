8ed025996be21b441d36e9f6873042e1e4a5fe03
Alex Orlet <AlexOrlet89@gmail.com>
2022-03-28 14:25:41 -0700

    dogs rendered on home

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { fetchDogs } from './services/DogList';
 
 export default function Home() {
-  fetchDogs();
-  return <div>ADOPT A DOG TODAY!</div>;
+  const [dogs, setDogs] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchDogs();
+      setDogs(data);
+    };
+    fetchData();
+  }, []);
+
+  console.log(dogs);
+
+  return (
+    <div>
+      <div>ADOPT A DOG TODAY!</div>
+      <div>
+        {dogs.map((dog) => (
+          <p key={dog.id}>{dog.name}</p>
+        ))}
+      </div>
+    </div>
+  );
 }
