907c73aa6984f51ab6f75979f788ca5a98610168
Alex Orlet <AlexOrlet89@gmail.com>
2022-03-30 12:05:13 -0700

    signin form added

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <div>
+      SignIn
+      <div>
+        <form>
+          <label>
+            {' '}
+            Email:
+            <input type="email" />
+          </label>
+          <label>
+            {' '}
+            Password
+            <input type="password" />
+          </label>
+          <button> SignIn </button>
+        </form>
+      </div>
+    </div>
+  );
 }
