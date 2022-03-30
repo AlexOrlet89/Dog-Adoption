832108a0d2fb9da230fc2aef97adc2c9d18b76a8
Alex Orlet <AlexOrlet89@gmail.com>
2022-03-30 12:41:02 -0700

    sign up user sends to supabase with email and password states set by form, confirmed with network tab and console log

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { signInUser, signupUser } from '../services/users';
 
 export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const resp = await signupUser({ email, password });
+    } catch {
+      setError('something went wrong!');
+    }
+  };
+
   return (
     <div>
       SignIn
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>
             {' '}
             Email:
-            <input type="email" />
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
           </label>
           <label>
             {' '}
             Password
-            <input type="password" />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
           </label>
           <button> SignIn </button>
         </form>
